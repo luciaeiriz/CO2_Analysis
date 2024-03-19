d6761fb920a09f1d6722ceac90302c7b0928ef2d
luciaeiriz <163864888+luciaeiriz@users.noreply.github.com>
2024-03-19 12:20:30 +0100

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -1,1 +1,394 @@
+import ROOT
+from ROOT import gROOT, TString
+from ROOT import TCanvas, TLine, TGaxis, TGraph, gPad, TF1, TF2, kRed, TMultiGraph, TLegend, gStyle, TPaveStats, TStyle, TText, TList, TLatex, TGraphErrors, TFile, TTree
+import json
+import math
+from array import array
+import numpy as np
+import pandas as pd
 
+histFile  = ROOT.TFile.Open("Scans/Scan_005565/Scan005565_HV10_CAEN.root","READ")
+
+sigma_factor = (6.2*10e-3)/1.5
+
+def HVCurrentDataFrame(scanIds):
+    current_top = []    
+    current_bot = []    
+    HV_top = []    
+    HV_bot = []
+    eff = []
+    eff_error = []
+    muon_stream = []
+    gamma_stream = []
+    muon_CS = []
+    gamma_CS = []
+    muon_CM = []
+    gamma_CM = []
+    muon_CS_err = []
+    gamma_CS_err = []
+    muon_CM_err = []
+    gamma_CM_err = []
+    noiseGammaRate = []
+    deltaV_top = []
+    deltaV_bot = []
+    deltaV_top_err = []
+    deltaV_bot_err = []
+    for scanId in scanIds:
+        !echo "None" >> counter.txt
+        !rm counter.txt
+        path = "Scans/Scan_00"+str(scanId)+"/*_HV*_CAEN.root"
+        !ls {path} >> counter.txt
+        counter = np.loadtxt("counter.txt", dtype=str)
+        if (str(scanId) == '5634') or (str(scanId) == '5630'):
+            print("scanId = ", scanId)
+            N = len(counter)
+        else:
+            N = len(counter)+1
+        for i in range(1,N):
+            jsonFile = open("Scans/Scan_00"+str(scanId)+"/ANALYSIS/KODELE/HV"+str(i)+"/output.json")
+            jsonFile = json.load(jsonFile)
+            #print(jsonFile['output_parameters']['efficiencyMuon_corrected'])
+            histFile  = ROOT.TFile.Open("Scans/Scan_00"+str(scanId)+"/Scan00"+str(scanId)+"_HV"+str(i)+"_CAEN.root","READ")
+            hist_current_bot = histFile.Get("Imon_KODELE-BOT")
+            hist_current_top = histFile.Get("Imon_KODELE-TOP")
+            hist_HV_bot = histFile.Get("HVeff_KODELE-BOT")
+            hist_HV_top = histFile.Get("HVeff_KODELE-TOP")
+            #eff.append(jsonFile['output_parameters']['efficiencyMuon_corrected'])
+            #eff_error.append((jsonFile['output_parameters']['efficiencyMuon_corrected']*(1-jsonFile['output_parameters']['efficiencyMuon_corrected'])/1000)**0.5)
+            eff.append(jsonFile['output_parameters']['efficiencyMuon_corrected'])
+            eff_error.append(jsonFile['output_parameters']['efficiencyMuon_corrected_err'])
+            muon_stream.append(jsonFile['output_parameters']['muonStreamerProbability'])
+            gamma_stream.append(jsonFile['output_parameters']['gammaStreamerProbability'])
+            muon_CS.append(jsonFile['output_parameters']['muonCLS'])
+            gamma_CS.append(jsonFile['output_parameters']['gammaCLS'])
+            muon_CM.append(jsonFile['output_parameters']['muonCMP'])
+            gamma_CM.append(jsonFile['output_parameters']['gammaCMP'])
+            muon_CS_err.append(jsonFile['output_parameters']['muonCLS_err'])
+            gamma_CS_err.append(jsonFile['output_parameters']['gammaCLS_err'])
+            muon_CM_err.append(jsonFile['output_parameters']['muonCMP_err'])
+            gamma_CM_err.append(jsonFile['output_parameters']['gammaCMP_err'])
+            noiseGammaRate.append(jsonFile['output_parameters']['noiseGammaRate'])
+            current_bot.append(hist_current_bot.GetMean())
+            current_top.append(hist_current_top.GetMean())
+            HV_top.append(hist_HV_top.GetMean())
+            HV_bot.append(hist_HV_bot.GetMean())
+            if (scanId == '5630'):
+                deltaV_top.append(0.001)
+                deltaV_bot.append(0.001)
+            else:
+                deltaV_top.append(hist_current_top.GetMean()*1.5)
+                deltaV_bot.append(hist_current_bot.GetMean()*1.5)
+            deltaV_top_err.append(hist_current_top.GetMean()*6.2*10e-3)
+            deltaV_bot_err.append(hist_current_bot.GetMean()*6.2*10e-3)
+            histFile.Close()
+    DataSet = {'HV_top': HV_top, 
+               'HV_bot': HV_bot, 
+               'current_top': current_top, 
+               'current_bot': current_bot,
+               'muon_stream': muon_stream,
+               'gamma_stream': gamma_stream,
+               'muon_CM': muon_CM,
+               'gamma_CM': gamma_CM,
+               'muon_CS': muon_CS,
+               'gamma_CS': gamma_CS,
+               'muon_CM_err': muon_CM_err,
+               'gamma_CM_err': gamma_CM_err,
+               'muon_CS_err': muon_CS_err,
+               'gamma_CS_err': gamma_CS_err,
+               'efficiency': eff,
+               'eff_error': eff_error,
+               'noiseGammaRate': noiseGammaRate,
+               'deltaV': (np.array(deltaV_top)*np.array(deltaV_bot)+np.full_like(np.array(deltaV_bot), 0.00000001))/(np.array(deltaV_top)+np.array(deltaV_bot)+np.full_like(np.array(deltaV_bot), 0.00000001)),
+               }
+    DataSet = pd.DataFrame(data=DataSet)
+    DataSet['current'] = DataSet['current_top'] + DataSet['current_bot']
+    DataSet['deltaV_err'] = DataSet['deltaV']* sigma_factor *np.sqrt( (1 + (np.sqrt(DataSet['current_top']**2 + DataSet['current_bot']**2 ) / ( DataSet['current_top']+DataSet['current_bot']  ) )**2 ) ) 
+    return DataSet
+        
+#print(HVCurrentDataFrame(5565))
+        
+#print(hist)
+#canvas = ROOT.TCanvas("canvas")
+#canvas.cd()
+#canvas.SetLogy(True)
+#hist.Draw("h")
+
+def FeaturesDataFrame(scanIds):
+    current_top = []    
+    current_bot = []    
+    eff = []
+    eff_error = []
+    muon_stream = []
+    gamma_stream = []
+    muon_CS = []
+    gamma_CS = []
+    muon_CM = []
+    gamma_CM = []
+    muon_CS_err = []
+    gamma_CS_err = []
+    muon_CM_err = []
+    gamma_CM_err = []
+    current_bot = []
+    current_top = []
+    HV_top = []
+    HV_bot = []
+    noiseGammaRate = []
+    noiseGammaRate_err = []
+    for scanId in scanIds:
+        #jsonFile = open("Scans/Scan_00"+str(scanIds[scanId][0])+"/ANALYSIS/KODELE/output.json")
+        print("scanID: ", scanIds[scanId][0])
+        
+        direc = ""
+        N = "1"
+        if (int(scanIds[scanId][0])==5810) or (int(scanIds[scanId][0])==5805) or (int(scanIds[scanId][0])==5820) or (int(scanIds[scanId][0])==5826) or (int(scanIds[scanId][0])==5830) or (int(scanIds[scanId][0])==5827) or (int(scanIds[scanId][0])==5815) or (int(scanIds[scanId][0])==5811):    
+            N = "7"
+        if (int(scanIds[scanId][0])==5841)or (int(scanIds[scanId][0])==5838) or (int(scanIds[scanId][0])==5840):
+            N = "8"
+        jsonFile = open("Scans/Scan_00"+str(scanIds[scanId][0])+"/ANALYSIS/KODELE/HV"+str(N)+"/output.json")
+        histFile  = ROOT.TFile.Open("Scans/Scan_00"+str(scanIds[scanId][0])+"/Scan00"+str(scanIds[scanId][0])+"_HV"+str(N)+"_CAEN.root","READ")
+        hist_current_bot = histFile.Get("Imon_KODELE-BOT")
+        hist_current_top = histFile.Get("Imon_KODELE-TOP")
+        hist_HV_bot = histFile.Get("HVeff_KODELE-BOT")
+        hist_HV_top = histFile.Get("HVeff_KODELE-TOP")
+        jsonFile = json.load(jsonFile)
+        #jsonFile_fornTrig = json.load(jsonFile_fornTrig)
+        nTrig = jsonFile['output_parameters']['nTrig']
+        #print(jsonFile['output_parameters']['efficiencyMuon_corrected'])
+        eff.append(100*jsonFile['output_parameters']['efficiencyMuon_corrected'])
+        eff_error.append(jsonFile['output_parameters']['efficiencyMuon_corrected_err'])
+        muon_stream.append(jsonFile['output_parameters']['muonStreamerProbability'])
+        gamma_stream.append(jsonFile['output_parameters']['gammaStreamerProbability'])
+        muon_CS.append(jsonFile['output_parameters']['muonCLS'])
+        gamma_CS.append(jsonFile['output_parameters']['gammaCLS'])
+        muon_CM.append(jsonFile['output_parameters']['muonCMP'])
+        gamma_CM.append(jsonFile['output_parameters']['gammaCMP'])
+        muon_CS_err.append(jsonFile['output_parameters']['muonCLS_err'])
+        gamma_CS_err.append(jsonFile['output_parameters']['gammaCLS_err'])
+        muon_CM_err.append(jsonFile['output_parameters']['muonCMP_err'])
+        gamma_CM_err.append(jsonFile['output_parameters']['gammaCMP_err'])
+        noiseGammaRate.append(jsonFile['output_parameters']['noiseGammaRate'])
+        noiseGammaRate_err.append(jsonFile['output_parameters']['noiseGammaRate_err'])
+        current_bot.append(hist_current_bot.GetMean())
+        current_top.append(hist_current_top.GetMean())
+        HV_top.append(hist_HV_top.GetMean())
+        HV_bot.append(hist_HV_bot.GetMean())
+        #print("N: ", N)
+        #print("direc: ", direc)
+        #print("current: ", hist_current_top.GetMean()+hist_current_bot.GetMean())
+        #print("HV: ", hist_HV_top.GetMean())
+    DataSet = {'current_top': current_top, 
+               'current_bot': current_bot,
+               'HV_top': HV_top,
+               'HV_bot': HV_bot,
+               'muon_stream': muon_stream,
+               'gamma_stream': gamma_stream,
+               'muon_CM': muon_CM,
+               'gamma_CM': gamma_CM,
+               'muon_CS': muon_CS,
+               'gamma_CS': gamma_CS,
+               'muon_CM_err': muon_CM_err,
+               'gamma_CM_err': gamma_CM_err,
+               'muon_CS_err': muon_CS_err,
+               'gamma_CS_err': gamma_CS_err,
+               'efficiency': eff,
+               'eff_error': eff_error,
+               'noiseGammaRate': noiseGammaRate,
+               'noiseGammaRate_err': noiseGammaRate_err
+              }
+        
+    DataSet = pd.DataFrame(data=DataSet)
+    DataSet['current'] = DataSet['current_top'] + DataSet['current_bot']
+    
+    return DataSet
+        
+#print(HVCurrentDataFrame(5565))
+        
+#print(hist)
+#canvas = ROOT.TCanvas("canvas")
+#canvas.cd()
+#canvas.SetLogy(True)
+#hist.Draw("h")
+
+sigmoid1 = TF1("sigmoid1","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid1.SetParNames("Emax","Lambda","HV50")
+sigmoid1.SetParameters(0.9, 0.01, 7000)
+sigmoid2 = TF1("sigmoid2","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid2.SetParNames("Emax","Lambda","HV50")
+sigmoid2.SetParameters(0.9, 0.01, 7000)
+sigmoid3 = TF1("sigmoid3","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid3.SetParNames("Emax","Lambda","HV50")
+sigmoid3.SetParameters(0.9, 0.01, 7000)
+sigmoid4 = TF1("sigmoid4","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid4.SetParNames("Emax","Lambda","HV50")
+sigmoid4.SetParameters(0.9, 0.01, 7000)
+sigmoid5 = TF1("sigmoid5","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid5.SetParNames("Emax","Lambda","HV50")
+sigmoid5.SetParameters(0.9, 0.001, 7000)
+sigmoid6 = TF1("sigmoid6","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid6.SetParNames("Emax","Lambda","HV50")
+sigmoid6.SetParameters(0.9, 0.01, 7000)
+sigmoid7 = TF1("sigmoid7","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid7.SetParNames("Emax","Lambda","HV50")
+sigmoid7.SetParameters(0.9, 0.01, 7000)
+sigmoid8 = TF1("sigmoid8","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid8.SetParNames("Emax","Lambda","HV50")
+sigmoid8.SetParameters(0.9, 0.01, 7000)
+sigmoid9 = TF1("sigmoid9","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid9.SetParNames("Emax","Lambda","HV50")
+sigmoid9.SetParameters(0.9, 0.001, 7000)
+sigmoid10 = TF1("sigmoid10","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid10.SetParNames("Emax","Lambda","HV50")
+sigmoid10.SetParameters(0.9, 0.01, 7000)
+sigmoid11 = TF1("sigmoid11","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid11.SetParNames("Emax","Lambda","HV50")
+sigmoid11.SetParameters(0.9, 0.0001, 11000)
+sigmoid12 = TF1("sigmoid12","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid12.SetParNames("Emax","Lambda","HV50")
+sigmoid12.SetParameters(0.9, 0.01, 7000)
+sigmoid13 = TF1("sigmoid13","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid13.SetParNames("Emax","Lambda","HV50")
+sigmoid13.SetParameters(0.9, 0.01, 7000)
+sigmoid14 = TF1("sigmoid14","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid14.SetParNames("Emax","Lambda","HV50")
+sigmoid14.SetParameters(0.9, 0.001, 7000)
+sigmoid15 = TF1("sigmoid15","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid15.SetParNames("Emax","Lambda","HV50")
+sigmoid15.SetParameters(0.9, 0.001, 7000)
+sigmoid16 = TF1("sigmoid16","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid16.SetParNames("Emax","Lambda","HV50")
+sigmoid16.SetParameters(0.9, 0.01, 7000)
+sigmoid17 = TF1("sigmoid17","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid17.SetParNames("Emax","Lambda","HV50")
+sigmoid17.SetParameters(0.9, 0.01, 7000)
+sigmoid18 = TF1("sigmoid18","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid18.SetParNames("Emax","Lambda","HV50")
+sigmoid18.SetParameters(0.9, 0.01, 7000)
+sigmoid19 = TF1("sigmoid19","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid19.SetParNames("Emax","Lambda","HV50")
+sigmoid19.SetParameters(0.9, 0.001, 7000)
+sigmoid20 = TF1("sigmoid20","([0]/(1+ TMath::Exp(-[1]*(x-[2]))))")
+sigmoid20.SetParNames("Emax","Lambda","HV50")
+sigmoid20.SetParameters(0.9, 0.001, 7000)
+
+scans = {'30CO2_OFF': ['5627'],
+         '30CO2_22':  ['5659'],
+         '30CO2_10':  ['5675'],
+         '30CO2_6.9': ['5621'],
+         '30CO2_4.6': ['5623'],
+         '30CO2_3.3': ['5622', '5625'],
+         '30CO2_2.2': ['5827'],
+         '30CO2_1': ['5820'],
+         '30CO205SF6_OFF': ['5712'],
+         '30CO205SF6_22':  ['5706'],
+         '30CO205SF6_10':  ['5707'],
+         '30CO205SF6_6.9': ['5708'],
+         '30CO205SF6_4.6': ['5709'],
+         '30CO205SF6_3.3': ['5713'],
+         '30CO205SF6_2.2': ['5840'],
+         '30CO205SF6_1': ['5838'],
+         '40CO2_OFF': ['5612'],
+         '40CO2_22':  ['5615'],
+         '40CO2_10':  ['5614'],
+         '40CO2_6.9': ['5616'],
+         '40CO2_4.6': ['5613'],
+         '40CO2_3.3': ['5611'],
+         'STDMX_OFF': ['5809'],
+         'STDMX_22':  ['5632'],
+         'STDMX_10':  ['5631'],
+         'STDMX_6.9': ['5633'],
+         'STDMX_4.6': ['5630'],
+         'STDMX_3.3': ['5629'],
+         'STDMX_2.2': ['5805'],
+         'STDMX_1': ['5810']
+         }
+Scans_30CO2 = {'30CO2_OFF': ['5819'],
+         '30CO2_22':  ['5826'],
+         '30CO2_10':  ['5832'], #HV7
+         '30CO2_6.9': ['5830'], #HV7
+         '30CO2_4.6': ['5831'],
+         '30CO2_3.3': ['5822'],
+         '30CO2_2.2': ['5827'], #HV7
+         '30CO2_1': ['5820'] #HV7
+         }
+Scans_40CO2 = {'40CO2_OFF': ['5845'],
+         '40CO2_22':  ['5846'],
+         '40CO2_10':  ['5844'],
+         '40CO2_6.9': ['5843'],
+         '40CO2_4.6': ['5842'],
+         '40CO2_3.3': ['5841'] # HV8
+         }
+Scans_STDMX = {'STDMX_OFF': ['5808'],
+         'STDMX_22':  ['5813'],
+         'STDMX_10':  ['5815'], #HV7
+         'STDMX_6.9': ['5811'], #HV7 
+         'STDMX_4.6': ['5807'],
+         'STDMX_3.3': ['5814'],
+         'STDMX_2.2': ['5805'], #HV7
+         'STDMX_1': ['5810'], #HV7
+         }
+Scans_30CO205SF6 = {'30CO205SF6_OFF': ['5833'],
+         '30CO205SF6_22':  ['5839'],
+         '30CO205SF6_10':  ['5836'],
+         '30CO205SF6_6.9': ['5837'],
+         '30CO205SF6_4.6': ['5835'],
+         '30CO205SF6_3.3': ['5834'],
+         '30CO205SF6_2.2': ['5838'], #HV7
+         '30CO205SF6_1': ['5840']} #HV7
+scans_STDMX = [
+         ['STDMX_OFF', sigmoid1, 6, 23],
+         ['STDMX_22', sigmoid2, 3, 22],
+         ['STDMX_4.6', sigmoid3, 1, 34],
+         ['STDMX_3.3', sigmoid4, 2, 20],
+         ['STDMX_1', sigmoid5, 4, 21],
+         ]
+scans_30CO2 = [
+         ['30CO2_OFF', sigmoid6, 6, 23],
+         ['30CO2_22', sigmoid7, 3, 22],
+         ['30CO2_4.6', sigmoid8, 1, 34],
+         ['30CO2_3.3', sigmoid9, 2, 20],
+         ['30CO2_1', sigmoid10, 4, 21],
+         ]
+scans_30CO205SF6 = [
+         ['30CO205SF6_OFF', sigmoid16, 6, 23],
+         ['30CO205SF6_10', sigmoid17, 3, 22],
+         ['30CO205SF6_4.6', sigmoid18, 1, 34],
+         ['30CO205SF6_2.2', sigmoid19, 2, 20],
+         ['30CO205SF6_1', sigmoid20, 4, 21],
+         ]
+scans_40CO2 = [
+         ['40CO2_OFF', sigmoid11, 6, 23],
+         ['40CO2_22', sigmoid12, 3, 22],
+         ['40CO2_10', sigmoid13, 1, 34],
+         ['40CO2_4.6', sigmoid14, 2, 20],
+         ['40CO2_3.3', sigmoid15, 4, 21],
+         ]
+
+for scan in scans:
+    print(scan, ': ')
+    print(HVCurrentDataFrame(scans[scan]))
+
+Features_STDMX = FeaturesDataFrame(Scans_STDMX)
+Features_STDMX['gamma_CM'][0] = 1
+Features_STDMX['gamma_CS'][0] = 1
+Features_STDMX['noiseGammaRate'][0] = 1
+print("Features_STDMX = ", Features_STDMX)
+
+Features_30CO2 = FeaturesDataFrame(Scans_30CO2)
+Features_30CO2['gamma_CM'][0] = 1
+Features_30CO2['gamma_CS'][0] = 1
+Features_30CO2['noiseGammaRate'][0] = 1
+print("FeaturesDataFrame(Scans_30CO2) = ", FeaturesDataFrame(Scans_30CO2))
+
+Features_30CO205SF6 = FeaturesDataFrame(Scans_30CO205SF6)
+Features_30CO205SF6['gamma_CM'][0] = 1
+Features_30CO205SF6['gamma_CS'][0] = 1
+Features_30CO205SF6['noiseGammaRate'][0] = 1
+print("FeaturesDataFrame(Scans_30CO205SF6) = ", FeaturesDataFrame(Scans_30CO205SF6))
+
+Features_40CO2 = FeaturesDataFrame(Scans_40CO2)
+Features_40CO2['gamma_CM'][0] = 1
+Features_40CO2['gamma_CS'][0] = 1
+Features_40CO2['noiseGammaRate'][0] = 1
+print("FeaturesDataFrame(Scans_40CO2) = ", FeaturesDataFrame(Scans_40CO2))
+
+#[In]12
