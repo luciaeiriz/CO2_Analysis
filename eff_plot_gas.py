ee1bdecc9d46d41d6dac1b2c9d9cde2e1e0681b7
luciaeiriz <163864888+luciaeiriz@users.noreply.github.com>
2024-08-23 16:20:38 +0200

    Update eff_plot_gas.py

--- eff_plot_gas.py
+++ eff_plot_gas.py
@@ -8,7 +8,7 @@ import numpy as np
 import math
 import mplhep as hep
 from extract_data import data
-from datetime import datetime
+
 
 """"
 Before running the scripit: 
@@ -32,11 +32,11 @@ font2 = {'family': 'DejaVu Sans',
 
 markers = ['o', '^', 's', '*', 'd']
 colors = ['black', 'magenta', 'red', 'blue', 'green']
-# mixtures = {"30CO2" : "64% TFE + 30% CO2 + 5% iC4H10 + 1% SF6",
-#             "30CO205SF6" : "64.5% TFE + 30% CO2 + 5% iC4H10 + 0.5% SF6",
-#             "40CO2" : "54% TFE + 40% CO2 + 5% iC4H10 + 1% SF6",
-#             "STDMX" : "95.2% TFE + 4.5% iC4H10 + 0.83% SF6"
-# }
+mixtures = {"30CO2" : "64% TFE + 30% CO2 + 5% iC4H10 + 1.0% SF6",
+            "30CO205SF6" : "64.5% TFE + 30% CO2 + 5% iC4H10 + 0.5% SF6",
+            "40CO2" : "54% TFE + 40% CO2 + 5% iC4H10 + 1.0% SF6",
+            "STDMX" : "95.2% TFE + 4.5% iC4H10 + 0.3% SF6"
+}
 
 def read_background_rates(filename):
     background_rates = {}
@@ -44,8 +44,8 @@ def read_background_rates(filename):
         reader = csv.reader(csvfile)
         next(reader)  # Skip the header row
         for row in reader:
-            scan_name, wp_str, background_rate_str = row
-            wp = float(wp_str) / 1000
+            scan_name,hv_str,current_top_str,current_bot_str,muoncs_str,gammacs_str,muoncs_err_str,gammacs_err_str,eff_str,eff_err,noiseGammaRate,current_str,background_rate_str,gamma_charge_str,gamma_charge_err_str=row
+            wp = float(hv_str) / 1000
             background_rate = float(background_rate_str)
             if scan_name not in background_rates:
                 background_rates[scan_name] = []
@@ -118,30 +118,30 @@ def plot_scan(scan):
 
     labels = []
     for wp, background_rate in background_rates.get(scan, []):
-        labels.append(f"bkg rate = {background_rate:.2f} Hz/cm2, WP = {b:.2f} kV, Eff(WP) = {c} %")
+        labels.append(f"bkg rate at WP= {background_rate:.2f} kHz/cm2, WP = {b:.2f} kV, Eff(WP) = {c} %")
     label = '\n'.join(labels)
-    plt.errorbar(HV, eff, yerr=err, fmt=marker, markersize=9, color=color, label=label)
+    plt.errorbar(HV, eff, yerr=err*100, fmt=marker, markersize=11, color=color, label=label)
 
     # if d < 0.01:
     #     label = f"no bkg rate, plateau = {a} %, WP = {b:.2f} kV, Eff(WP) = {c} %"
-    #     plt.errorbar(HV, eff, yerr=err, fmt=marker, markersize=9, color=color, label=label)
+    #     plt.errorbar(HV, eff, yerr=err, fmt=marker, markersize=11, color=color, label=label)
     # else:
     #     label = f"bkg rate = {d:.1f} Hz/cm2, plateau = {a} %, WP = {b:.2f} kV, Eff(WP) = {c} %"
-    #     plt.errorbar(HV, eff, yerr=err, fmt=marker, markersize=9, color=color, label=label)
+    #     plt.errorbar(HV, eff, yerr=err, fmt=marker, markersize=11, color=color, label=label)
 
     x = np.linspace(min(HV), max(HV), 100)
     y = func(x, E, L, H)
     plt.plot(x, y, linewidth=3, color=color)
 
-background_rates = read_background_rates('background_rates.csv')
+background_rates = read_background_rates('2024_data_atWP.csv')
 plot_scan_colors = {}
 plot_scan_markers = {}
 
 #Setting the plot
 hep.style.use("CMS")
 figure, ax = plt.subplots(figsize=(13, 13))
-plt.xlabel(r'HV$_{\mathrm{eff}}$ [kV]', fontsize=36)
-plt.ylabel('Muon efficiency [%]', fontsize=36)
+plt.xlabel(r'HV$_{\mathrm{eff}}$ [V]', fontsize=36)
+plt.ylabel('Muon efficiency', fontsize=36)
 plt.grid(ls='--')
 
 ax.xaxis.set_major_locator(AutoLocator())
@@ -152,7 +152,7 @@ ax.tick_params(which='major', direction='in', length=10, width=2.0, labelsize=12
 ax.tick_params(which='minor', direction='in', length=5, width=1.0)
 plt.yticks(fontproperties='DejaVu Sans', size=20, weight='bold')  
 plt.xticks(fontproperties='DejaVu Sans', size=20, weight='bold') 
-plt.xlim(6050, 7550)
+plt.xlim(6300, 7600)
 plt.ylim(0, 1.2)
 
 # Input scan names from user
@@ -167,14 +167,20 @@ if not os.path.exists('Plots_2024/'):
     os.makedirs('Plots_2024/')
 
 # Plotting 
-label = "54% TFE + 40% CO2 + 5% iC4H10 + 1% SF6" #change label accordingly to mixture/ ABS
-ax.text(0.025, 0.75, label + "\nTest Beam April 2024 \nThreshold: 60fC \n1.4 mm double gap RPC", transform=ax.transAxes, verticalalignment='top', horizontalalignment='left', fontsize=16)
+prefixes = set(scan.split('_')[0].strip() for scan in scans)
+mixture_labels = [mixtures.get(prefix, f"Unknown Mixture ({prefix})") for prefix in prefixes]
 
-plt.text(7300, 1.21+0.01, "GIF++", font0)
+# Generate label based on scan names
+label = " + ".join(set(mixture_labels))
+label += "\nTest Beam April 2024 \nThreshold: 60 fC \n1.4 mm double gap RPC"
+ax.text(0.035, 0.78, label, transform=ax.transAxes, verticalalignment='top', horizontalalignment='left', fontsize=19, linespacing=1.5)
+
+plt.text(7400, 1.21+0.01, "GIF++", font0)
 hep.cms.text("Preliminary", fontsize=32)
 plt.legend(loc='upper left', prop=font2, frameon=False)
 plt.axhline(y=1, color='black', linestyle='--')
 plt.savefig(os.path.join('Plots_2024/' + name + ".png"))
 
 #plt.show()
+
 
